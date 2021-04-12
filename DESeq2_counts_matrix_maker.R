1409f40cf81f37674f4701c91022d29370d82f78
William Montgomery <montgomerywf@nih.gov>
2021-04-12 15:05:51 -0400

    This commit adds the libraries necessary for these commands.

--- DESeq2_counts_matrix_maker.R
+++ DESeq2_counts_matrix_maker.R
@@ -1,7 +1,8 @@
 ##how to make a counts file from DESeq2
+library(DESeq2)
+library(writexl)
 
-
-#load the data in 
+#load the data in
 
 directory <- "/Users/montgomerywf/Desktop/Sen Colab Files/NKT_RNA_seq/RNAseq_2018/reverse_counts/"
 
@@ -28,5 +29,5 @@ dds_2018 <- DESeq(dds_2018)
 counts_df <- counts(dds_2018, normalized = TRUE)
 counts_df <- as.data.frame(counts_df, row.names = rownames(counts_df))
 counts_df$Genes <- rownames(counts_df)
-write_xlsx(x = counts_df, path = "~/Desktop/Sen Colab Files/NKT_RNA_seq/RNAseq_2018/reverse_results/RNAseq_2018_reverse_counts.xlsx", 
+write_xlsx(x = counts_df, path = "~/Desktop/Sen Colab Files/NKT_RNA_seq/RNAseq_2018/reverse_results/RNAseq_2018_reverse_counts.xlsx",
            format_headers = TRUE)
